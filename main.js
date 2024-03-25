0d7fe2c185323dde9c018562fd38fb1542e71d6d
Steffen T <steffmturner94@gmail.com>
2024-03-24 21:01:48 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello, Steffen!");
+console.log("Steffen Turner");
+console.log("21094");
+console.log("Steffen Turner", "21094");
+console.log("Steffen");
+console.log("Turner");
+console.log("21094");
+console.log("Steffen" + "Turner" + "21094");
+console.log("Steffen "+"Turner "+"21094");
